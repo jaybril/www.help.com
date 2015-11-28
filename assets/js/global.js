c8f4609e9f2ab784418388e7c0df1afeb6f2166d
jaybril <jaybril@163.com>
2015-11-29 00:39:44 +0800

    more

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,15 @@
 /**
  * Created by jaybril on 15/11/26.
  */
+var showMyCart=function(){
+    //$(" .mycart-ul").stop();
+    //$(" .mycart-rrow").stop();
+    $(" .mycart-rrow").show();
+    $(" .mycart-ul").show();
+}
+var hideMyCart=function(){
+    //$(" .mycart-ul").stop();
+    //$(" .mycart-rrow").stop();
+    $(" .mycart-rrow").hide();
+    $(" .mycart-ul").hide();
+}
