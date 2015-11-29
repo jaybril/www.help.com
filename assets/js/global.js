6720e193f96174aa0a1a70755383ffd516300a83
jaybril <jaybril@163.com>
2015-11-29 21:36:45 +0800

    edw

--- assets/js/global.js
+++ assets/js/global.js
@@ -13,3 +13,50 @@ var hideMyCart=function(){
     $(" .mycart-rrow").hide();
     $(" .mycart-ul").hide();
 }
+
+
+var orderCenterTab=function(){
+    layer.tab({
+        area:['600px','300px'],
+        tab:[
+            {
+            title:'全部订单',
+            content:'全部订单'
+            },
+            {
+                title:'抢单中',
+                content:'抢单中'
+            },
+            {
+                title:'进行中',
+                content:'进行中'
+            },
+            {
+                title:'已完成',
+                content:'已完成'
+            },
+            {
+                title:'问题件',
+                content:'问题件'
+            }
+        ]
+    });
+}
+
+/*取消订单-btn*/
+var cancelOrder=function(){
+    layer.confirm(
+        '确定取消订单吗？',
+        {btn:['是','否']},
+        function(){
+
+        },
+        function(){
+
+        }
+    )
+}
+/*订单改价*/
+var changeOrderPrice=function(){
+
+}
